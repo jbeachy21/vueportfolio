be103c90d0ee2775f4b1f5e407d9ca364d5c808d
Jasper Beachy <jbeach17@gmail.com>
2020-05-19 13:55:38 -0500

    updated intro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,41 +16,26 @@
 
     <v-card-text><p class="display-2 text--primary">Jasper Beachy</p>
     
-    <p class="title text--secondary">Full Stack Web Developer</p>
+    <p class="title text--secondary"> Web Developer</p>
     
     </v-card-text>
       
     <v-card-text >
-    <p class="body-1">Growing up in Silicon Valley in Palo Alto, California and being the son of two Stanford University 
-      researchers, I’ve been exposed to and have participated in computer programming and research from an early age.  
-      This manifested itself into a passion for computer programming and the actual mechanics of computers.  
-      To that end, I graduated from Gustavus Adolphus College majoring in Chemistry, minoring in Scandinavian studies 
-      and having completed coursework in the C, C++, Python, Java, and Matlab programming languages.</p>
+    <p class="body-1">I have been interested in science, computers, and technology in general since 
+      before I can remember.  If I were however, to pinpoint a time when my interest was first sparked 
+      it would be when my brother and I first started to play our Nintendo 64.  Since then, my interest 
+      has skyrocketed, in middle school and high school I started experimenting with the terminal on my 
+      computer, by the end of high school I was taking an intro to computer science concepts class and my 
+      freshman year of college I took an introduction to computer science course taught in python.  
+      Although, I did not major in computer science but in chemistry because I wanted a science 
+      background as well. After college, I attended the Full Stack Web Development Coding Bootcamp 
+      at the University of Minnesota and graduated from that program in May 2019.  Since starting my bootcamp 
+      I have made numerous web applications, relearned python, interned at a cryptocurrency startup, and 
+      even tinkered with a raspberry pi, (late to the party yeah I know) making a retropie. I am now very 
+      excited for my first full time development opportunity and making my dream a reality. 
+    </p>
       <br>
-    <p class="body-1">Since then, I have attended and completed a Full Stack Web Development Coding Bootcamp 
-      at the University of Minnesota.  In this 6-month course, I completed coursework on various relevant 
-      technologies including JavaScript, Node.js, MySQL, MongoDB, HTML, CSS and the React framework.  
-      Since completing that Coding Bootcamp, I have continued to learn on my own working to master other technologies 
-      like Ruby, SCSS, TypeScript and Angular.</p>
-    <br>
-    <p class="body-1">During my time at my Coding Bootcamp, I created a good number of SPAs 
-      (Single Page Applications) as well as larger projects in Agile-like team settings.  
-      My personal favorite was an application titled “Jamster”, which at its MVP is supposed to be a platform 
-      for matching musicians with others in their area wishing to jam with each other.  This project used MySQL 
-      (for authentication), Trello as the project board, React, React bootstrap, npm, Express, CSS, and was 
-      deployed on Heroku.</p>
-      <br>
-
-    <p class="body-1">Most recently, I was a Frontend Developer intern at cryptocurrency startup, 
-      DeFiner, where I worked on an Agile team, using Jira as our project board.  In this role, I further 
-      developed my skillset with Angular in particular and began to work in a TDD 
-      (Test Driven Development) environment using Jasmine and Karma.  Additionally, I further 
-      built on my knowledge of JavaScript and Node by using TypeScript in conjunction with the Angular 
-      front end framework.</p>
-    <br>
-    <p class="body-1">Since completing my most recent role at DeFiner, I have been searching for a full time role.
-    If you wish to schedule a time to chat, please use the calendly link below or use the links below to shoot me
-    an email or to connect on LinkedIn.</p>
+    
       </v-card-text>   
     
 
