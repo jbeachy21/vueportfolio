68eb9313264f69b007c8e0a11632a88d9ccb85ab
Jasper Beachy <jbeach17@gmail.com>
2020-05-20 15:57:41 -0500

    intro edited images added

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ npm install
 ### Compiles and hot-reloads for development
 ```
 npm run serve
+
+or 
+
+./node_modules/.bin/vue-cli-service serve
 ```
 
 ### Compiles and minifies for production
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,23 +16,25 @@
 
     <v-card-text><p class="display-2 text--primary">Jasper Beachy</p>
     
-    <p class="title text--secondary"> Web Developer</p>
+    <p class="title text--secondary">Web Developer</p>
     
     </v-card-text>
       
     <v-card-text >
-    <p class="body-1">I have been interested in science, computers, and technology in general since 
-      before I can remember.  If I were however, to pinpoint a time when my interest was first sparked 
-      it would be when my brother and I first started to play our Nintendo 64.  Since then, my interest 
-      has skyrocketed, in middle school and high school I started experimenting with the terminal on my 
-      computer, by the end of high school I was taking an intro to computer science concepts class and my 
-      freshman year of college I took an introduction to computer science course taught in python.  
-      Although, I did not major in computer science but in chemistry because I wanted a science 
-      background as well. After college, I attended the Full Stack Web Development Coding Bootcamp 
-      at the University of Minnesota and graduated from that program in May 2019.  Since starting my bootcamp 
-      I have made numerous web applications, relearned python, interned at a cryptocurrency startup, and 
-      even tinkered with a raspberry pi, (late to the party yeah I know) making a retropie. I am now very 
-      excited for my first full time development opportunity and making my dream a reality. 
+    <p class="body-1">  
+    My name is Jasper Beachy, an aspiring Web Developer with a passion 
+    for solving real world problems using science and technology, currently seeking my first 
+    full time opportunity.  I have been coding in some form since high school but formally started
+    to learn how to program in college where I learned languages such as Python, C, C++, Java, and Matlab.
+    Within the last year, I graduated from the University of Minnesota Full Stack Web Development bootcamp.
+    At my bootcamp, I learned relevant Web Development technologies such as HTML, CSS, Git, JavaScript, jQuery, 
+    Node, npm, React, RESTful APIs, MySQL, and MongoDb making numerous single page applications and a few larger 
+    projects along the way.  After my bootcamp I took on a part time internship at a cryptocurrency startup 
+    called DeFiner.  At my internship I got exposure to new tools such as SCSS, Angular, and TypeScript.  
+    Since my time with DeFiner ended I have gone on to learn more on my own.  For example, the portfolio you 
+    are looking at was designed from scratch in Vue.  Additionally, I went on to relearn Python on Udemy and have
+    even gone to investigate Deno, (version 1.0) made by Ryan Dahl, who created Node.  Now I am very eagerly 
+    searching for my first full time development opportunity.      
     </p>
       <br>
     
@@ -43,6 +45,12 @@
       
        
     </v-card-actions>
+    <v-row justify="space-around">
+      
+    <img src="../assets/originalcert.png" alt="Certificate image" >
+    <img src="../assets/udemycert_original.jpg" alt="Certificate image">
+     
+    </v-row>
   </v-card>
 
 
